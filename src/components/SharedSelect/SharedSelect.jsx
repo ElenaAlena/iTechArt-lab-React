937c6ba8ed5707cef5c657fd1d73086bacbd9df1
AlenaNovik <elena.pigaleva@gmail.com>
2021-07-29 07:59:45 +0300

    Replace 404 to public folder

---
+++ src/components/SharedSelect/SharedSelect.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import Select from "@material-ui/core/Select";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from '@material-ui/core/FormControl';
+import { useTheme } from "@material-ui/core/styles";
+
+import useStyles from "./styled";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  "Oliver Hansen",
+  "Van Henry",
+  "April Tucker",
+  "Ralph Hubbard",
+  "Omar Alexander",
+  "Carlos Abbott",
+  "Miriam Wagner",
+  "Bradley Wilkerson",
+  "Virginia Andrews",
+  "Kelly Snyder",
+];
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+const SharedSelect = () => {
+  const [personName, setPersonName] = useState([]);
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const handleChange = (event) => {
+    setPersonName(event.target.value);
+  };
+  return (
+    <React.Fragment>
+    <FormControl className={classes.formControl}>
+      <InputLabel id="demo-mutiple-name-label">Share to</InputLabel>
+      <Select
+        className={classes.formControl}
+        labelId="demo-mutiple-name-label"
+        id="demo-mutiple-name"
+        multiple
+        value={personName}
+        onChange={handleChange}
+        input={<Input />}
+        MenuProps={MenuProps}
+      >
+        {names.map((name) => (
+          <MenuItem
+            key={name}
+            value={name}
+            style={getStyles(name, personName, theme)}
+          >
+            {name}
+          </MenuItem>
+        ))}
+      </Select>
+      </FormControl>
+    </React.Fragment>
+
+  );
+};
+
+export default SharedSelect;
